b9c327956d3f97fb193ccfb4287208f421962abe
MaevaBriolais <briolais.maeva.pro@gmail.com>
2024-07-10 21:00:42 +0200

    add css for menu-burger

--- client/src/components/navBar.jsx
+++ client/src/components/navBar.jsx
@@ -1,10 +1,18 @@
 import "../styles/navBar.css"
 import avatar from "../assets/images/avatar.png"
+import { useState } from "react";
 
 function NavBar() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
  return (
     <header className="navbar">
-  <span className="menu-burger">
+  <span className="menu-burger" onClick={toggleMenu}>
     <span></span>
     <span></span>
     <span></span>
@@ -13,6 +21,11 @@ function NavBar() {
   <div className="login">
     <img src={avatar} alt="avatar login"/>
   </div>
+  <nav className={`menu ${isOpen ? 'open' : ''}`}>
+    <ul>
+      <li><a href="">Pâtisserie</a></li>
+    </ul>
+  </nav>
 </header>
     );
   }
